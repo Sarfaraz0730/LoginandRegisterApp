70f367e75e553a4b9ee76e8dbefc243bc6c043eb
=Sarfaraz0730 <72138410+Sarfaraz0730@users.noreply.github.com>
2022-08-06 12:36:49 +0530

    Register page created succesffuly

---
+++ BackEnd/index.js
@@ -0,0 +1,63 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from "mongoose"
+// const express = require("express")
+const app = express();
+ app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017/aurasofLoginRegisterDb", {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}, () => {
+    console.log("Connected")
+});
+
+//Schema
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password:String
+})
+//Model
+
+const User = new mongoose.model("User", userSchema)
+//Routes
+
+app.post("/login", (req, res) => {
+    res.send("My api Login")
+})
+
+
+app.post("/register", (req, res) => {
+//   console.log(req.body)
+    const { name, email, password } = req.body;
+
+    User.findOne({ email: email }, (err, user) => {
+        if (user) {
+            res.send({message : "User already register"})
+        } else {
+            const user = new User({
+              name,
+              email,
+              password,
+            });
+
+            user.save((err) => {
+              if (err) {
+                res.send(err);
+              } else {
+                res.send({ message: "Successfully Register" });
+              }
+            });
+        }
+    })
+    
+});
+
+
+app.listen(12345, () => {
+    console.log("Listening on Port : 12345")
+})
